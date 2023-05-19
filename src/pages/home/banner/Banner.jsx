45fb70b4785937aa2729813d951e4d87f47eaa93
rupokraihan <rupok2321@gmail.com>
2023-05-19 13:54:49 +0600

    banner title and description implemented

--- src/pages/home/banner/Banner.jsx
+++ src/pages/home/banner/Banner.jsx
@@ -1,19 +1,24 @@
-import React from 'react';
-import banner from '../../../assets/banner/tbzz-polybags-shell-2.jpg'
+import React from "react";
+import banner from "../../../assets/banner/tbzz-polybags-shell-2.jpg";
 
 const Banner = () => {
   return (
     <div className="hero lg:mt-0">
       <div className="bg-opacity-25">
-      <img className="w-100%" src={banner} alt="" />
+        <img className="w-100%" src={banner} alt="" />
       </div>
-      <div className="hero-content text-center text-neutral-content">
-        <div className="max-w-md mt-44">
-          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-          <p className="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+      <div className="hero-content text-neutral-content">
+        <div className="max-w-2xl mt-72">
+          <h1 className="mb-5 text-7xl font-bold tracking-widest">
+            Journey into toyHaven
+          </h1>
+          <p className="mb-5 tracking-widest">
+            Welcome to toyHaven Ville, where the world of toy cars comes to
+            life! Explore a vast selection of sports cars, trucks, regular cars,
+            mini fire trucks, mini police cars, and more. Fuel your imagination
+            and embark on thrilling adventures with our high-quality toy car
+            collection. Get ready fun in the ultimate toy car wonderland -
+            toyHaven!
           </p>
           <button className="btn btn-primary">Get Started</button>
         </div>
